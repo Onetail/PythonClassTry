2347f82ffd4a2fe7fcb562c6a00c6f7e6e1e5cf0
Onetail <paosong91@gmail.com>
2017-10-26 21:44:05 +0800

    trying to use linketlist

--- factory.py
+++ factory.py
@@ -8,8 +8,10 @@ class Factory:
         def __init__(self,uid,name="None"):
                 self.uid = Global.UID
                 self.name = name
+                self.next = None
                 Global.UID += 1
-                
+        
+        #       to print class value
         def oUtput(self,text=""):
                 text += "\t"+str(self.uid)+"\t\n"
                 text += "\t"+self.name+"\t\n"
@@ -19,13 +21,18 @@ class ProductA(Factory):
                 super(self,ProductA).__init__(name)
                 self.name = name
                 self.value = value
-                self.next = null
+
         
         def uPdate(self,name=self.name,value=self.value):
                 self.name = name
                 self.value =  value 
         
-        def 
+        #       to overwrite print
+        def oUtput(self,text=""):
+                text+= "\t" +str(self.uid)+"\t\n"
+                text+= "\t" +str(self.name)+"\t\n"
+                text+= "\t" +str(self.value)+"\t\n"
+                return print(text)
                 
                 
 
