3d2e94c42de816238f768515ecd95b145f2cd893
Onetail <paosong91@gmail.com>
2017-11-09 22:34:58 +0800

    nodejs get txt savefilename

--- assets/javascript/server.js
+++ assets/javascript/server.js
@@ -3,13 +3,13 @@ const app = require('express')();
 const http = require("http").createServer(app)
 const fs = require("fs")
 
-// const path = require("path")
-// app.use(bodyparser());
-FILEDATA = ""   
-HTMLDATA = ""  
+
+FILEDATA = ""   //..python string
+HTMLDATA = ""  //..web string  
 SERVEROPENCLOSE = false
 
 IMAGESARRAY = ["title.png","background.png","backgrounddark.png","login.png","close.png","searchbar.png"] //  圖片array
+FILEARRAY = []   // to get 'Package' for know how many file to save
 
 mAinrunning = new function()    // init fs and array
 {
@@ -28,8 +28,14 @@ mAinrunning = new function()    // init fs and array
     {
         IMAGESARRAY.push(i+".jpg")
     }
-
-    fs.readFile("../../index.html",'utf8',(err,data)=>
+    fs.readFile("../../data/Package","utf-8",(err,data)=>
+    {
+        if(err) throw err 
+        data = data.split(" ")
+        for(var i = 0; i<data.length-1;i++){FILEARRAY.push(data[i])}
+        fIleurlset()
+    })
+    fs.readFile("../../index.html",'utf-8',(err,data)=>
     {
         if (err) throw err 
         HTMLDATA = data
@@ -38,6 +44,7 @@ mAinrunning = new function()    // init fs and array
         // res.sendFile("index.html" , {root:'../../'})
         fs.readFile("../../data/data.txt",'utf-8',(err,data)=>
         {
+            if(err) throw err
             FILEDATA = data
             HTMLDATA += data
             fs.readFile("../../bottomlayout.html","utf-8",(err,data)=>
@@ -46,43 +53,61 @@ mAinrunning = new function()    // init fs and array
             })
         })
         
-    })    
+    }) 
+    console.log(FILEARRAY)
+    hTmlurlset()
 }
-
-app.get('/', (req, res)=>
+function hTmlurlset()
 {
-
-    res.setHeader('Content-Type', 'text/html');
-    res.send(HTMLDATA)
-    res.end()
-       
-});
-app.post("/pydata",(req,res)=>
-{
-    res.send(FILEDATA)
-})
-app.post("/classimage",(req,res)=>
-{
-    res.send(IMAGESARRAY)   
-})
-app.get("/assets/css/index.css",(req,res)=>
-{
-    res.sendFile("index.css",{root:'../../assets/css/'})
-})
-app.get("/assets/javascript/feature.js",(req,res)=>
-{
-    res.sendFile(__dirname+"/feature.js")
-})
-
-
-for(let i = 0;i<IMAGESARRAY.length;i++)
-{
-    app.get("/assets/images/"+IMAGESARRAY[i],(req,res)=>
+    app.get('/', (req, res)=>
     {
-        res.sendFile(IMAGESARRAY[i],{root:'../../assets/images'})
+    
+        res.setHeader('Content-Type', 'text/html');
+        res.send(HTMLDATA)
+        res.end()
+           
+    });
+    app.post("/pydata",(req,res)=>
+    {
+        res.send(FILEDATA)
     })
+    app.post("/classimage",(req,res)=>
+    {
+        res.send(IMAGESARRAY)   
+    })
+    app.get("/assets/css/index.css",(req,res)=>
+    {
+        res.sendFile("index.css",{root:'../../assets/css/'})
+    })
+    app.get("/assets/javascript/feature.js",(req,res)=>
+    {
+        res.sendFile(__dirname+"/feature.js")
+    })
+    
+    for(let i = 0;i<IMAGESARRAY.length;i++)
+    {
+        app.get("/assets/images/"+IMAGESARRAY[i],(req,res)=>
+        {
+            res.sendFile(IMAGESARRAY[i],{root:'../../assets/images'})
+        })
+    }
+    
 }
-
+function fIleurlset()
+{
+    for(let i =0;i<FILEARRAY.length;i++)
+    {
+        fs.readFile("../../data/"+FILEARRAY[i],"utf-8",(err,data)=>
+        {
+            if(err) throw err
+            app.get("/data/"+FILEARRAY[i],(req,res)=>
+            {
+                res.send(data)
+            })
+        })
+        
+    }
+}
 //指定port
 // var server = https.createServer(options, App).listen(3000,function()
 // {
