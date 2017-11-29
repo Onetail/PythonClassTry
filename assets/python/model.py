899cc20350b9c3b9eeed7c6de8297d02b24bcbd1
Onetail <paosong91@gmail.com>
2017-11-29 09:28:26 +0800

    update model message origin if else , now use switch

--- assets/python/model.py
+++ assets/python/model.py
@@ -140,44 +140,84 @@ class Model:
 		Global.LOOPMODEL = False
 
 	def mOdelmessage(self,type,data=""):
-		string = ""
-		string += "\n\tBuild Linkedlist model success!\t" if type == 1 else ""
-		string += "\n\tmodel is exist!\t" if type == 2 else ""
-		string += "\n\tPlease first to build model (use >'build')\t" if type == 3 else ""
-		string += "\n\tPlease enter two value for 'name' and 'value'!" if type == 4 else ""
-		string += "\n\tPersonnel Name:{:} Value:{:} add to model!".format(data[0],data[1]) if type == 5 else ""
-		string += "\n\tDelete Name:{:} from model".format(data) if type == 6 else ""
 		
-		string += "\n\tPlease Enter correct command\t" if type == 7 else ""
-		string += "\n\tadd\t: to add a new personnel in model ."if type == 7 else ""
-		string += "\n\tdelete\t: to delete a personnel from model ."if type == 7 else ""
-		string += "\n\tupdate\t: to update already exist personnel's value ."if type == 7 else ""
-		string += "\n\tprint\t: to print detail from model ." if type == 7 else ""
-		string += "\n\tsave (name)\t: to save the file in data ." if type == 7 else ""
-		string += "\n\trandom\t: to random 10 number in linkedlist ." if type == 7 else ""
-		string += "\n\twrite\t: to write line into file and save it ." if type == 7 else ""
-		string += "\n\tupload\t: to upload local data to server ." if type == 7 else ""
+		switch = {
+			1: lambda: "\n\tBuild Linkedlist model success!\t",
+			2: lambda: "\n\tmodel is exist!\t",
+			3: lambda: "\n\tPlease first to build model (use >'build')\t",
+			4: lambda: "\n\tPlease enter two value for 'name' and 'value'!",
+			5: lambda: "\n\tPersonnel Name:{:} Value:{:} add to model!".format(data[0],data[1]),
+			6: lambda: "\n\tDelete Name:{:} from model".format(data),
+			7: lambda: "\n\tPlease Enter correct command\t" \
+			+ "\n\tadd\t: to add a new personnel in model ." \
+			+ "\n\tdelete\t: to delete a personnel from model ." \
+			+ "\n\tupdate\t: to update already exist personnel's value ." \
+			+ "\n\tprint\t: to print detail from model ."  \
+			+ "\n\tsave (name)\t: to save the file in data ." \
+			+ "\n\trandom\t: to random 10 number in linkedlist ." \
+			+ "\n\twrite\t: to write line into file and save it ." \
+			+ "\n\tupload\t: to upload local data to server .",				
+			
+			8: lambda: "\n\tAlready save data in file . \t",
+			9: lambda: "\n\tInsert into 10 random number . \t",
+			10: lambda: "\n\tCannot use Package to filename . \t",
+			11: lambda: "\n\tExit write line model . \t",
+			12: lambda: "\n{:10} \t".format(data),
+			13: lambda: "\n\tError! The file already have mode .",
+			14: lambda: "\n\tlist\t: to read file content ." \
+			+ "\n\tsave (mode)\t: to save to Package in Browser ." \
+			+ "\n\tupper\t: remove end line in file and print it ." \
+			+ "\n\tremove\t: remove file mode ." \
+			+ "\n\tclear\t: clear file data ." ,
+			15: lambda: "\n\tSuccess! remove mode in file .",
+			16: lambda: "\n\tError! the file no have mode .",
+			17: lambda: "\n\tAlready clear the '{:}' file .".format(data),
+			18: lambda: "\n\tSuccess! upload new detail to Server .",
+			19: lambda: "\n\tError! Connection come up with error to server .",
+			20: lambda: "\n\tSuccess! {:5} the server".format(data),
+			"ename": "\n\tError! the Name is not exist ."
+			
+		}
+		# type =str(type).upper()
+		return print(switch.get(type)())
+		# string = ""
+		# string += "\n\tBuild Linkedlist model success!\t" if type == 1 else ""
+		# string += "\n\tmodel is exist!\t" if type == 2 else ""
+		# string += "\n\tPlease first to build model (use >'build')\t" if type == 3 else ""
+		# string += "\n\tPlease enter two value for 'name' and 'value'!" if type == 4 else ""
+		# string += "\n\tPersonnel Name:{:} Value:{:} add to model!".format(data[0],data[1]) if type == 5 else ""
+		# string += "\n\tDelete Name:{:} from model".format(data) if type == 6 else ""
+		
+		# string += "\n\tPlease Enter correct command\t" if type == 7 else ""
+		# string += "\n\tadd\t: to add a new personnel in model ."if type == 7 else ""
+		# string += "\n\tdelete\t: to delete a personnel from model ."if type == 7 else ""
+		# string += "\n\tupdate\t: to update already exist personnel's value ."if type == 7 else ""
+		# string += "\n\tprint\t: to print detail from model ." if type == 7 else ""
+		# string += "\n\tsave (name)\t: to save the file in data ." if type == 7 else ""
+		# string += "\n\trandom\t: to random 10 number in linkedlist ." if type == 7 else ""
+		# string += "\n\twrite\t: to write line into file and save it ." if type == 7 else ""
+		# string += "\n\tupload\t: to upload local data to server ." if type == 7 else ""
 
-		string += "\n\tAlready save data in file . \t" if type == 8 else ""
-		string += "\n\tInsert into 10 random number . \t" if type == 9 else ""
-		string += "\n\tCannot use Package to filename . \t" if type == 10 else ""
-		string += "\n\tExit write line model . \t" if type == 11 else ""
-		string += "\n{:10} \t".format(data) if type == 12 else ""
-		string += "\n\tError! The file already have mode ." if type == 13 else "" # file have mode do this
+		# string += "\n\tAlready save data in file . \t" if type == 8 else ""
+		# string += "\n\tInsert into 10 random number . \t" if type == 9 else ""
+		# string += "\n\tCannot use Package to filename . \t" if type == 10 else ""
+		# string += "\n\tExit write line model . \t" if type == 11 else ""
+		# string += "\n{:10} \t".format(data) if type == 12 else ""
+		# string += "\n\tError! The file already have mode ." if type == 13 else "" # file have mode do this
 
-		string += "\n\tlist\t: to read file content ." if type == 14 else ""
-		string += "\n\tsave (mode)\t: to save to Package in Browser ." if type == 14 else ""
-		string += "\n\tupper\t: remove end line in file and print it ." if type == 14 else ""
-		string += "\n\tremove\t: remove file mode ." if type == 14 else ""
-		string += "\n\tclear\t: clear file data ." if type == 14 else ""
+		# string += "\n\tlist\t: to read file content ." if type == 14 else ""
+		# string += "\n\tsave (mode)\t: to save to Package in Browser ." if type == 14 else ""
+		# string += "\n\tupper\t: remove end line in file and print it ." if type == 14 else ""
+		# string += "\n\tremove\t: remove file mode ." if type == 14 else ""
+		# string += "\n\tclear\t: clear file data ." if type == 14 else ""
 
-		string += "\n\tSuccess! remove mode in file ." if type == 15 else ""
-		string += "\n\tError! the file no have mode ." if type == 16 else "" 
-		string += "\n\tAlready clear the '{:}' file .".format(data) if type == 17 else ""
-		string += "\n\tSuccess! upload new detail to Server ." if type  == 18 else ""
-		string += "\n\tError! Connection come up with error to server ." if type == 19 else "" 
-		string += "\n\tSuccess! {:5} the server".format(data) if type == 20 else ""
+		# string += "\n\tSuccess! remove mode in file ." if type == 15 else ""
+		# string += "\n\tError! the file no have mode ." if type == 16 else "" 
+		# string += "\n\tAlready clear the '{:}' file .".format(data) if type == 17 else ""
+		# string += "\n\tSuccess! upload new detail to Server ." if type  == 18 else ""
+		# string += "\n\tError! Connection come up with error to server ." if type == 19 else "" 
+		# string += "\n\tSuccess! {:5} the server".format(data) if type == 20 else ""
 
-		string += "\n\tError! the Name is not exist ." if str(type).upper() == "ENAME" else ""
-		return print(string)
+		# string += "\n\tError! the Name is not exist ." if str(type).upper() == "ENAME" else ""
+		# return print(string)
 	
